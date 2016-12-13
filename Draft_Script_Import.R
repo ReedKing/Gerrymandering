4d4a84756eadcb086caee6f239794f0d392cf238
ReedKing <reed.e.king@gmail.com>
2016-12-13 14:10:23 -0500

    Census data joined with spatial working

--- Draft_Script_Import.R
+++ Draft_Script_Import.R
@@ -13,9 +13,8 @@ library(maptools)
 
 tract <- readOGR(dsn = ".", layer = "cb_2015_37_tract_500k")
 tract@data$GEOID <- as.character(tract@data$GEOID)
-tract_tidy <- tidy(tract, region = "GEOID")
-tract_tidy$Id <- tract_tidy$id
-tract_tidy$id <- NULL
+tract <- tidy(tract, region = "GEOID")
+
 
 NCData <- read.csv("ACS_15_5YR_S0601.csv", stringsAsFactors = FALSE)
 colnames(NCData) <- as.character(unlist(NCData[1,]))
@@ -23,21 +22,20 @@ NCData <- NCData[-1,]
 NCData$Id <- NCData$Id2
 NCData$Id2 <- NULL
 
-## GGPlot2 Map ----
+Main_Data <- fortify(tract, region = "GEOID")
+Main_Data$Id <- Main_Data$id
+Main_Data$id <- NULL
+Main_Data <- left_join(Main_Data, NCData, by = c("Id"))
 
-maptract <- fortify(tract_tidy, region = "GEOID")
-maptract <- left_join(maptract, NCData, by = c("Id"))
+Main_Data[,9:432] <- lapply(Main_Data[,9:432], function(x){
+  as.numeric(levels(x))[x]
+  })
 
-maptract <- maptract[grep("Alamance | Alexander | Alleghany | Anson | Ashe | Avery | Beaufort | Bertie | Blanden | Brunswick | Buncombe | Burke | Cabarrus | Caldwell | Camden
-                         | Carteret | Caswell | Catawba | Chatham | Cherokee | Chowan | Clay | Cleveland | Columbus | Craven | Cumberland | Currituck | Dare | Davidson 
-                         | Davie | Duplin | Durham | Edgecombe | Forsyth | Franklin | Gaston | Gates | Graham | Granville | Greene | Guilford | Halifax | Harnett | Haywood 
-                         | Henderson | Hertford | Hoke | Hyde | Iredell | Jackson | Johnston | Jones | Lee | Lenoir | Linconl | McDowell | Macon | Madison | Martin | Mecklenburg
-                         | Mithcell | Montgomery | Moore | Nash | New Hanover | Northampton | Onslow | Orange | Pamlico | Pasquotank | Pender | Perquimans | Person | Pitt | Polk
-                         | Randolph | Richmond | Robeson | Rockingham | Rowan | Rutherford | Sampson | Scotland | Stanly | Stokes | Surry | Swain | Transylvania | Tyrrell | Union
-                         | Vance | Wake | Warren | Washington | Watauga | Wayne | Wilkes | Wilson | Yadkin | Yancey", maptract$Geography),]
+## Map to Test ----
+
 
 ggplot() + 
-  geom_polygon(data = maptract, aes(x = long, y = lat, group = group), color = "grey50")
+  geom_polygon(data = Main_Data, aes(x = long, y = lat, group = group), color = "grey50")
   
 
   
