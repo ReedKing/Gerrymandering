afd195500c40cebcc167820e23f83a74f4c9ff5c
ReedKing <reed.e.king@gmail.com>
2016-12-18 21:54:41 -0500

    Fix bad mergerino

--- Import_Tigris.R
+++ Import_Tigris.R
@@ -17,13 +17,8 @@ library(leaflet)
 
 # set key, grab tract spatial and geotag data
 api.key.install(key = "1a66d0d3b2a73f7877417f78b20796654ee0e6ff")
-<<<<<<< HEAD
 tigcounties <- counties(state = "NC", cb = TRUE)
-NCgeo <- geo.make(state = "NC", county = "*")
-=======
-tigtracts <- tracts(state = "NC", cb = TRUE)
-NCgeo <- geo.make(state = c("NC", county = "*", tract = "*"))
->>>>>>> 6abbddfe7cd8c1e5c4c5bcb5c4e9fe49ec884369
+NCgeo <- geo.make(state = "NC", county = c(1:100))
 
 # Querying for income
 
@@ -32,7 +27,6 @@ medianincome <- acs.fetch(endyear = 2015, span = 1, geography = NCgeo,
                        table.number = "B06011", col.names = "pretty")
 attr(medianincome, "acs.colnames")
 
-<<<<<<< HEAD
 # Now we know what the column is called, so make a df
 
 medianincome_df <- data.frame(paste0(str_pad(medianincome@geography$state, 2, "left", pad = "0"),
@@ -43,6 +37,7 @@ medianincome_df <- data.frame(paste0(str_pad(medianincome@geography$state, 2, "l
 
 rownames(medianincome_df) <- 1:nrow(medianincome_df)
 names(medianincome_df) <- c("GEOID", "medianincome")
+medianincome_df$GEOID <- c(tigcounties@data$GEOID)
 
 # Merge
 
@@ -50,34 +45,13 @@ medianincome_merged <- geo_join(tigcounties, medianincome_df, "GEOID", "GEOID")
 medianincome_merged <- medianincome_merged[medianincome_merged$ALAND > 0,]
 
 ## Map ----
-=======
-medianincome_df <- data.frame(paste0(str_pad(medianincome@geography$state, 2, "left", pad="0"), 
-                              str_pad(medianincome@geography$county, 3, "left", pad="0"), 
-                              str_pad(medianincome@geography$tract, 6, "left", pad="0")), 
-                       medianincome@estimate[,c("B06011. Median Income in the Past 12 Months (in 2015 Inflation-Adjusted Dollars) by Place of Birth in the United States: Median income in the past 12 months -- Total:")], 
-                       stringsAsFactors = FALSE)
-
-medianincome_df <- select(medianincome_df, 1:2)
-rownames(medianincome_df) <- 1:nrow(medianincome_df)
-names(medianincome_df) <- c("GEOID", "medianincome")
-
-medianincome_merged <- geo_join(tigtracts, medianincome_df, "GEOID", "GEOID")
-medianincome_merged <- medianincome_merged[medianincome_merged$ALAND > 0,]
-
-
->>>>>>> 6abbddfe7cd8c1e5c4c5bcb5c4e9fe49ec884369
 mappop <- paste0("GEOID: ", medianincome_merged$GEOID, "<br>", "Median Income: ", medianincome_merged$medianincome, "$")
 pal <- colorNumeric(
   palette = "YlGnBu",
   domain = medianincome_df$medianincome
 )
 
-<<<<<<< HEAD
-
-medianincomecountiesmap <- leaflet() %>%
-=======
 medianincomemap <- leaflet() %>%
->>>>>>> 6abbddfe7cd8c1e5c4c5bcb5c4e9fe49ec884369
   addProviderTiles("CartoDB.Positron") %>%
   addPolygons(data = medianincome_merged,
               fillColor = ~pal(medianincome),
@@ -91,8 +65,3 @@ medianincomemap <- leaflet() %>%
             position = "bottomright",
             title = "Median Income",
             labFormat = labelFormat(suffix = "$"))
-<<<<<<< HEAD
-medianincomecountiesmap
-=======
-medianincomemap
->>>>>>> 6abbddfe7cd8c1e5c4c5bcb5c4e9fe49ec884369
