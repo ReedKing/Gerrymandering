3d0114777e12b078537ae13e0ca8c460cb6385d8
ReedKing <reed.e.king@gmail.com>
2016-12-18 23:32:26 -0500

    Tigris Import of Median Income Working

--- Import_Tigris.R
+++ Import_Tigris.R
@@ -18,21 +18,22 @@ library(leaflet)
 
 # set key, grab tract spatial and geotag data
 api.key.install(key = "1a66d0d3b2a73f7877417f78b20796654ee0e6ff")
-tigcounties <- counties(state = "NC", cb = TRUE)
-NCgeo <- geo.make(state = "NC", county = "*")
+tigdata <- tracts(state = "NC", cb = TRUE)
+NCgeo <- geo.make(state = "NC", county = "*", tract = "*")
 
 # Querying for income
 
-incometablenums <- acs.lookup(keyword = "median income", endyear = 2015, case.sensitive = FALSE)
+incometablenums <- acs.lookup(keyword = "income", endyear = 2015, case.sensitive = FALSE)
 medianincome <- acs.fetch(endyear = 2015, span = 5, geography = NCgeo,
-                       table.number = "B06011", col.names = "pretty")
+                       table.number = "B19013", col.names = "pretty")
 attr(medianincome, "acs.colnames")
 
 # Now we know what the column is called, so make a df
 
 medianincome_df <- data.frame(paste0(str_pad(medianincome@geography$state, 2, "left", pad = "0"),
-                                     str_pad(medianincome@geography$county, 3, "left", pad = "0")),
-                              medianincome@estimate[,c("B06011. Median Income in the Past 12 Months (in 2015 Inflation-Adjusted Dollars) by Place of Birth in the United States: Median income in the past 12 months -- Total:")], 
+                                     str_pad(medianincome@geography$county, 3, "left", pad = "0"),
+                                     str_pad(medianincome@geography$tract, 6, "left", pad = "0")),
+                              medianincome@estimate[,c("B19013. Median Household Income in the Past 12 Months (in 2015 Inflation-Adjusted Dollars): Median household income in the past 12 months (in 2015 Inflation-adjusted dollars)")], 
                               stringsAsFactors = FALSE)
 # Clean
 
@@ -42,11 +43,11 @@ names(medianincome_df) <- c("GEOID", "medianincome")
 
 # Merge
 
-medianincome_merged <- geo_join(tigcounties, medianincome_df, "GEOID", "GEOID")
+medianincome_merged <- geo_join(tigdata, medianincome_df, "GEOID", "GEOID")
 medianincome_merged <- medianincome_merged[medianincome_merged$ALAND > 0,]
 
 ## Map ----
-mappop <- paste0("GEOID: ", medianincome_merged$COUNTYNS, "<br>", "Median Income: ", medianincome_merged$medianincome, "$")
+mappop <- paste0("Tract :", medianincome_merged$NAME, "<br>", "Median Income: ", medianincome_merged$medianincome, "$")
 pal <- colorNumeric(
   palette = "YlGnBu",
   domain = medianincome_df$medianincome
